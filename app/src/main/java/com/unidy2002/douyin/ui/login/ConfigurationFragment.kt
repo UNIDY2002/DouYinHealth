37075d6ae95c9cb16b6572458f08cbaeb6ccc678
UNIDY2002 <UNIDY2002@outlook.com>
2021-08-27 22:02:31 +0800

    Implement configuration behavior

--- app/src/main/java/com/unidy2002/douyin/ui/login/ConfigurationFragment.kt
+++ app/src/main/java/com/unidy2002/douyin/ui/login/ConfigurationFragment.kt
@@ -1,11 +1,15 @@
 package com.unidy2002.douyin.ui.login
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.unidy2002.douyin.MainActivity
+import com.unidy2002.douyin.R
 import com.unidy2002.douyin.databinding.FragmentConfigurationBinding
+import com.unidy2002.douyin.models.currentUser
 
 class ConfigurationFragment : Fragment() {
 
@@ -21,6 +25,28 @@ class ConfigurationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentConfigurationBinding.inflate(inflater, container, false)
+        binding.configurationTimeText.setOnClickListener {
+            binding.configurationTimeText.setTextColor(resources.getColor(R.color.grey, null))
+            binding.configurationTimeSplit.setBackgroundColor(resources.getColor(R.color.grey, null))
+            binding.configurationFraudText.setTextColor(resources.getColor(R.color.light_grey, null))
+            binding.configurationFraudSplit.setBackgroundColor(resources.getColor(R.color.light_grey, null))
+            binding.configurationTimeGroup.visibility = View.VISIBLE
+            binding.configurationFraudGroup.visibility = View.GONE
+        }
+
+        binding.configurationFraudText.setOnClickListener {
+            binding.configurationFraudText.setTextColor(resources.getColor(R.color.grey, null))
+            binding.configurationFraudSplit.setBackgroundColor(resources.getColor(R.color.grey, null))
+            binding.configurationTimeText.setTextColor(resources.getColor(R.color.light_grey, null))
+            binding.configurationTimeSplit.setBackgroundColor(resources.getColor(R.color.light_grey, null))
+            binding.configurationFraudGroup.visibility = View.VISIBLE
+            binding.configurationTimeGroup.visibility = View.GONE
+        }
+
+        binding.configurationActionButton.setOnClickListener {
+            currentUser = "visitor"
+            startActivity(Intent(activity, MainActivity::class.java))
+        }
         return binding.root
     }
 
