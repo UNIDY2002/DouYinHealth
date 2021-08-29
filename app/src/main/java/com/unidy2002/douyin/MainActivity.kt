ab3016ce94a1f3d77544e2f55dde6a5f8222651b
UNIDY2002 <UNIDY2002@outlook.com>
2021-08-29 12:17:55 +0800

    Refactor bottom navigation

--- app/src/main/java/com/unidy2002/douyin/MainActivity.kt
+++ app/src/main/java/com/unidy2002/douyin/MainActivity.kt
@@ -3,14 +3,29 @@ package com.unidy2002.douyin
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 import com.unidy2002.douyin.databinding.ActivityMainBinding
 import com.unidy2002.douyin.models.currentUser
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private var focus = 0
+
+    private val targets
+        get() = listOf(
+            binding.bottomNavItemHome,
+            binding.bottomNavItemMe,
+        )
+
+    private fun changeFocus(target: Int) {
+        targets[focus].background = null
+        targets[target].background =
+            ResourcesCompat.getDrawable(resources, R.drawable.bottom_nav_selected_background, null)
+        focus = target
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,15 +34,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         (supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as? NavHostFragment)?.run {
-            NavigationUI.setupWithNavController(binding.navView, navController)
+            binding.bottomNavItemHome.setOnClickListener {
+                changeFocus(0)
+                navController.navigate(R.id.navigation_home)
+            }
 
-            binding.navView.setOnItemSelectedListener {
-                if (it.itemId != R.id.navigation_home && currentUser == null) {
+            binding.bottomNavItemMe.setOnClickListener {
+                if (currentUser == null) {
                     startActivity(Intent(this@MainActivity, LoginActivity::class.java))
-                    false
                 } else {
-                    navController.navigate(it.itemId)
-                    true
+                    changeFocus(1)
+                    navController.navigate(R.id.navigation_me)
                 }
             }
         }
