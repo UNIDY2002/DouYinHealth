f76a926b85b04eb0914e7341f58ddc8d668f83c5
UNIDY2002 <UNIDY2002@outlook.com>
2021-08-30 16:08:45 +0800

    Add a naive animation for health report

--- app/src/main/java/com/unidy2002/douyin/ui/health/HealthFragment.kt
+++ app/src/main/java/com/unidy2002/douyin/ui/health/HealthFragment.kt
@@ -1,5 +1,7 @@
 package com.unidy2002.douyin.ui.health
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import com.unidy2002.douyin.R
 import com.unidy2002.douyin.databinding.FragmentHealthBinding
+import java.util.*
 
 class HealthFragment : Fragment() {
 
@@ -27,6 +30,58 @@ class HealthFragment : Fragment() {
         binding.healthExchangeButton.setOnClickListener {
             NavHostFragment.findNavController(this).navigate(R.id.navigation_exchange)
         }
+
+        binding.healthHeaderContainer.animate().alpha(1f).setDuration(1000)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    binding.healthBarTitle.animate().alpha(1f).setDuration(1000)
+                        .setListener(object : AnimatorListenerAdapter() {
+                            override fun onAnimationEnd(animation: Animator?) {
+                                binding.healthBarGraph.animate().alpha(1f).setDuration(1000)
+                                    .setListener(object : AnimatorListenerAdapter() {
+                                        override fun onAnimationEnd(animation: Animator?) {
+                                            binding.healthLineTitle.animate().alpha(1f).setDuration(1000)
+                                                .setListener(object : AnimatorListenerAdapter() {
+                                                    override fun onAnimationEnd(animation: Animator?) {
+                                                        binding.healthLineGraph.animate().alpha(1f).duration = 1000
+                                                    }
+                                                })
+                                        }
+                                    })
+                            }
+                        })
+                }
+            })
+
+        Timer(true).schedule(object : TimerTask() {
+            override fun run() {
+                activity?.runOnUiThread {
+                    binding.healthReportTitle.animate().alpha(1f).setDuration(1000)
+                        .setListener(object : AnimatorListenerAdapter() {
+                            override fun onAnimationEnd(animation: Animator?) {
+                                binding.healthReportSubtitle.animate().alpha(1f).setDuration(1000)
+                                    .setListener(object : AnimatorListenerAdapter() {
+                                        override fun onAnimationEnd(animation: Animator?) {
+                                            binding.healthReportRow1.animate().alpha(1f).setDuration(1000)
+                                                .setListener(object : AnimatorListenerAdapter() {
+                                                    override fun onAnimationEnd(animation: Animator?) {
+                                                        binding.healthReportRow2.animate().alpha(1f).setDuration(1000)
+                                                            .setListener(object : AnimatorListenerAdapter() {
+                                                                override fun onAnimationEnd(animation: Animator?) {
+                                                                    binding.healthReportRow3.animate()
+                                                                        .alpha(1f).duration = 1000
+                                                                }
+                                                            })
+                                                    }
+                                                })
+                                        }
+                                    })
+                            }
+                        })
+                }
+            }
+        }, 5400)
+
         return binding.root
     }
 
